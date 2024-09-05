c7c7e591b3770e91bca3ea0c44066c3d83701bf4
Ian Pearce <ian.pearce@babylist.com>
2024-09-05 14:13:23 -0700

    Update periodic job syncing specs and add failing spec for JSON job options

--- spec/sidekiq/cronitor/periodic_jobs_spec.rb
+++ spec/sidekiq/cronitor/periodic_jobs_spec.rb
@@ -2,12 +2,70 @@
 
 require 'sidekiq/cronitor/periodic_jobs'
 
+class DummyWorker
+  def self.sidekiq_options=(options)
+    @sidekiq_options ||= {}
+    @sidekiq_options.merge!(options)
+  end
+  def self.sidekiq_options
+    @sidekiq_options ||= {}
+  end
+  def self.reset_options
+    @sidekiq_options = {}
+  end
+end
+
 RSpec.describe Sidekiq::Cronitor::PeriodicJobs do
+  let(:cronitor_key) { "dummy-worker" }
+  let(:loops) { [job] }
+
   before do
-    allow(Sidekiq::Periodic::LoopSet).to receive(:new).and_return([])
+    class_double("Sidekiq::Periodic::LoopSet").as_stubbed_const
+    allow(Sidekiq::Periodic::LoopSet).to receive(:new).and_return(loops)
   end
 
   describe '.sync_schedule!' do
-    xit { expect { described_class.sync_schedule! }.not_to raise_error }
+    context "when no loops" do
+      let(:loops) { [] }
+      it { expect { described_class.sync_schedule! }.not_to raise_error }
+    end
+
+    context "when job options are stringified JSON" do
+      let(:job) {
+        instance_double(
+          "Sidekiq::Periodic::Loop",
+          klass: DummyWorker.name,
+          schedule: "* * * * *",
+          tz_name: "Etc/UTC",
+          options: {
+            "cronitor_key" => cronitor_key,
+            "cronitor_group" => "dummy-team"
+          }.to_json
+        )
+      }
+      it "updates monitors" do
+        expect(Cronitor::Monitor).to receive(:put).with(monitors: [hash_including(key: cronitor_key)])
+        described_class.sync_schedule!
+      end
+    end
+
+    context "when job options are a hash" do
+      let(:job) {
+        instance_double(
+          "Sidekiq::Periodic::Loop",
+          klass: DummyWorker.name,
+          schedule: "* * * * *",
+          tz_name: "Etc/UTC",
+          options: {
+            "cronitor_key" => cronitor_key,
+            "cronitor_group" => "dummy-team"
+          }
+        )
+      }
+      it "updates monitors" do
+        expect(Cronitor::Monitor).to receive(:put).with(monitors: [hash_including(key: cronitor_key)])
+        described_class.sync_schedule!
+      end
+    end
   end
 end
